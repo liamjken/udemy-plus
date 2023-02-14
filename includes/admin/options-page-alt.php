df76822bfc743e51c1e4166d7bc38f6b33677c62
liamjken <liamjken@gmail.com>
2023-02-14 09:07:26 -0500

    update

--- includes/admin/options-page-alt.php
+++ includes/admin/options-page-alt.php
@@ -3,7 +3,13 @@
 function up_plugin_options_alt_page() {
     ?>
 <div class="wrap">
-    Options page
+    <form method="POST" action="options.php">
+        <?php 
+    settings_fields('up_options_group');
+    do_settings_sections('up-options-page');
+    submit_button();
+        ?>
+    </form>
 </div>
     <?php
 }
