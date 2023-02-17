6e0ea864e80b4f2bb5dc02aef9ec78bdda7c811b
liamjken <liamjken@gmail.com>
2023-02-17 11:31:46 -0500

    update

---
+++ build/block-editor/index.js
@@ -0,0 +1,290 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/block-editor/neon.css":
+/*!***********************************!*\
+  !*** ./src/block-editor/neon.css ***!
+  \***********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "@wordpress/block-editor":
+/*!*************************************!*\
+  !*** external ["wp","blockEditor"] ***!
+  \*************************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["blockEditor"];
+
+/***/ }),
+
+/***/ "@wordpress/components":
+/*!************************************!*\
+  !*** external ["wp","components"] ***!
+  \************************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["components"];
+
+/***/ }),
+
+/***/ "@wordpress/data":
+/*!******************************!*\
+  !*** external ["wp","data"] ***!
+  \******************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["data"];
+
+/***/ }),
+
+/***/ "@wordpress/edit-post":
+/*!**********************************!*\
+  !*** external ["wp","editPost"] ***!
+  \**********************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["editPost"];
+
+/***/ }),
+
+/***/ "@wordpress/element":
+/*!*********************************!*\
+  !*** external ["wp","element"] ***!
+  \*********************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["element"];
+
+/***/ }),
+
+/***/ "@wordpress/i18n":
+/*!******************************!*\
+  !*** external ["wp","i18n"] ***!
+  \******************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["i18n"];
+
+/***/ }),
+
+/***/ "@wordpress/plugins":
+/*!*********************************!*\
+  !*** external ["wp","plugins"] ***!
+  \*********************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["plugins"];
+
+/***/ }),
+
+/***/ "@wordpress/rich-text":
+/*!**********************************!*\
+  !*** external ["wp","richText"] ***!
+  \**********************************/
+/***/ (function(module) {
+
+module.exports = window["wp"]["richText"];
+
+/***/ }),
+
+/***/ "./src/block-editor/neon-format.js":
+/*!*****************************************!*\
+  !*** ./src/block-editor/neon-format.js ***!
+  \*****************************************/
+/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _neon_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./neon.css */ "./src/block-editor/neon.css");
+/* harmony import */ var _wordpress_rich_text__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/rich-text */ "@wordpress/rich-text");
+/* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/block-editor */ "@wordpress/block-editor");
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
+
+
+
+
+
+
+(0,_wordpress_rich_text__WEBPACK_IMPORTED_MODULE_2__.registerFormatType)("udemy-plus/neon", {
+  title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Neon", "udemy-plus"),
+  tagName: "span",
+  className: "neon",
+  edit(_ref) {
+    let {
+      isActive,
+      onChange,
+      value
+    } = _ref;
+    const selectedBlock = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_5__.useSelect)(select => select("core/block-editor").getSelectedBlock());
+    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (selectedBlock === null || selectedBlock === void 0 ? void 0 : selectedBlock.name) === "core/paragraph" && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_3__.RichTextToolbarButton, {
+      title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Neon", "udemy-plus"),
+      icon: "superhero",
+      isActive: isActive,
+      onClick: () => {
+        onChange((0,_wordpress_rich_text__WEBPACK_IMPORTED_MODULE_2__.toggleFormat)(value, {
+          type: "udemy-plus/neon"
+        }));
+      }
+    }));
+  }
+});
+
+/***/ }),
+
+/***/ "./src/block-editor/sidebar.js":
+/*!*************************************!*\
+  !*** ./src/block-editor/sidebar.js ***!
+  \*************************************/
+/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/plugins */ "@wordpress/plugins");
+/* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/edit-post */ "@wordpress/edit-post");
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
+/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
+/* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @wordpress/block-editor */ "@wordpress/block-editor");
+
+
+
+
+
+
+
+(0,_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__.registerPlugin)("up-sidebar", {
+  render() {
+    const {
+      og_title,
+      og_image,
+      og_description,
+      og_override_image
+    } = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.useSelect)(select => {
+      return select("core/editor").getEditedPostAttribute("meta");
+    });
+    const {
+      editPost
+    } = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.useDispatch)("core/editor");
+    return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__.PluginSidebar, {
+      name: "up_sidebar",
+      icon: "share",
+      title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Udemy Plus Sidebar", "udemy-plus")
+    }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_5__.PanelBody, {
+      title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Opengraph Options", "udemy-plus")
+    }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_5__.TextControl, {
+      label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Title", "udemy-plus"),
+      value: og_title,
+      onChange: og_title => editPost({
+        meta: {
+          og_title
+        }
+      })
+    }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_5__.TextareaControl, {
+      label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Description", "udemy-plus"),
+      value: og_description,
+      onChange: og_description => {
+        editPost({
+          meta: {
+            og_description
+          }
+        });
+      }
+    }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_5__.ToggleControl, {
+      label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Override Featured Image", "udemy-plus"),
+      checked: og_override_image,
+      help: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("By default, the featured image will be used as the image. Check this option to use a different image.", "udemy-plus"),
+      onChange: og_override_image => {
+        editPost({
+          meta: {
+            og_override_image
+          }
+        });
+      }
+    }), og_override_image && (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
+      src: og_image
+    }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_6__.MediaUploadCheck, null, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_6__.MediaUpload, {
+      accept: ["image"],
+      render: _ref => {
+        let {
+          open
+        } = _ref;
+        return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_5__.Button, {
+          isPrimary: true,
+          onClick: open
+        }, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Change Image", "udemy-plus"));
+      },
+      onSelect: image => {
+        editPost({
+          meta: {
+            og_image: image.sizes.opengraph.url
+          }
+        });
+      }
+    })))));
+  }
+});
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+/*!***********************************!*\
+  !*** ./src/block-editor/index.js ***!
+  \***********************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _sidebar_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sidebar.js */ "./src/block-editor/sidebar.js");
+/* harmony import */ var _neon_format_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./neon-format.js */ "./src/block-editor/neon-format.js");
+
+
+}();
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -1,40 +1,37 @@
 <?php
 
 function up_register_blocks() {
-    $blocks = [
-        [ 'name' => 'fancy-header' ], 
-        [ 'name' => 'search-form', 'options' => [
-            'render_callback' => 'up_search_form_render_cb'
-        ] ],
-        [ 'name' => 'page-header', 'options' => [
-            'render_callback' => 'up_page_header_render_cb'
-        ]],
-        [ 'name' => 'header-tools', 'options' => [
-            'render_callback' => 'up_header_tools_render_cb'
-        ]],
-        [ 'name' => 'auth-modal', 'options' => [
-            'render_callback' => 'up_auth_modal_render_cb'
-        ]],
-        [ 'name' => 'recipe-summary', 'options' => [
-            'render_callback' => 'up_recipe_summary_render_cb'
-        ]],
-        [ 'name' => 'team-members-group'],
-        [ 'name' => 'team-member'],
-        [ 'name' => 'popular-recipes', 'options' => [
-          'render_callback' => 'up_popular_recipes_cb'  
-        ]],
-        ['name' => 'daily-recipe', 'options' => [
-         'render_callback' => 'up_daily_recipe_cb'   
-        ]]
+  $blocks = [
+    [ 'name' => 'fancy-header' ],
+    [ 'name' => 'search-form', 'options' => [
+      'render_callback' => 'up_search_form_render_cb'
+    ]],
+    [ 'name' => 'page-header', 'options' => [
+      'render_callback' => 'up_page_header_render_cb'
+    ]],
+    [ 'name' => 'header-tools', 'options' => [
+      'render_callback' => 'up_header_tools_render_cb'
+    ]],
+    [ 'name' => 'auth-modal', 'options' => [
+      'render_callback' => 'up_auth_modal_render_cb'
+    ]],
+    [ 'name' => 'recipe-summary', 'options' => [
+      'render_callback' => 'up_recipe_summary_render_cb'
+    ]],
+    [ 'name' => 'team-members-group' ],
+    [ 'name' => 'team-member' ],
+    [ 'name' => 'popular-recipes', 'options' => [
+      'render_callback' => 'up_popular_recipes_cb'
+    ]],
+    [ 'name' => 'daily-recipe', 'options' => [
+      'render_callback' => 'up_daily_recipe_cb'
+    ]]
+  ];
 
-    ];
-
-    foreach($blocks as $block) {
-        register_block_type(
-            UP_PLUGIN_DIR . 'build/blocks/' . $block['name'],
-            isset($block['options']) ? $block['options'] : []
-        );
-    }
-
- 
+  foreach($blocks as $block) {
+    register_block_type(
+      UP_PLUGIN_DIR . 'build/blocks/' . $block['name'],
+      isset($block['options']) ? $block['options'] : []
+    );
+  }
 }
